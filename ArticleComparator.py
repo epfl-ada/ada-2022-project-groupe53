a4d1782419aaa677a7d3daad34524a5ca605f503
MezghaniMehdi25 <mehdi.mezghani@epfl.ch>
2022-12-13 20:26:55 +0100

    insight about strategy

--- ArticleComparator.py
+++ ArticleComparator.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
+import torch
+from sentence_transformers import SentenceTransformer
 
+from transformers import BertTokenizer, BertModel
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.tokenize import WordPunctTokenizer
@@ -20,32 +23,20 @@ The compare_articles method takes in two articles and returns their similarity a
 
 class ArticleComparator :
 
-    def __init__(self,plain_text_path, article_list, nb_topics ):
+    def __init__(self,plain_text_path, article_list ):
         """
         @param plain_text_path: String, the path to the folder containing the articles
         @param article_list: List of Strings, the list of articles to be considered when building the model
         @param nb_topics: Integer, the number of topics to be considered when building the model
         """
-        # Initialize the lemmatizer, stop words, tokenizer and normalizer 
-        # used to clean the text
-        self.lemmatizer = WordNetLemmatizer()
-        self.stop_words = set(stopwords.words('english'))
-        self.tokenizer=WordPunctTokenizer()
-        self.text_processor = TextPreProcessor(
-                # Terms that will be normalized 
-                normalize=['email', 'percent', 'money', 'phone', 'user','time', 'url', 'date', 'number'],
-                # Terms that will be annotated
-                tokenizer=self.tokenizer.tokenize
-        )
-
         # Initialize the path from which the articles will be read
         self.plain_text_path = plain_text_path
         # Initialize the list of articles to be considered whe building the model
         self.article_list = article_list
-        # Initialize the dataframe of articles and their preprocessed text
+        # Initialize the bert model to be used for embedding
+        self.bert_model =  SentenceTransformer('bert-base-nli-mean-tokens')
+        # Initialize the dataframe of articles and their text and embeddings
         self.articles_df = self.build_article_df()
-        # Initialize the LDA model
-        self.lda = self.build_lda_model(nb_topics = nb_topics)
 
     def build_article_df (self):
         """
@@ -53,7 +44,7 @@ class ArticleComparator :
         """
 
         # Create a dataframe with the articles as index and the text as column
-        articles_df = pd.DataFrame(columns=['text'])
+        articles_df = pd.DataFrame(columns=['text', 'article_embedding', 'title_embedding'])
 
         # Go through the articles and add them to the dataframe
         for article in self.article_list :
@@ -66,46 +57,35 @@ class ArticleComparator :
                     text = text.split("Related subjects:")[1]
                     text = text.split("\n\n")[1]
 
-                    #Add the article to the dataframe after preprocessing the text
-                    articles_df.loc[article] = self.preprocess_text( text)
+                    #Add the article to the dataframe after preprocessing the text, and computing the embedding
+                    articles_df.loc[article] = [text , self.bert_model.encode(text), self.bert_model.encode(article.replace("_", " "))]
         return articles_df
     
-    def build_lda_model(self, nb_topics = 10):
-        """
-        @param nb_topics: Integer, the number of topics to be considered when building the model
-        @return: LatentDirichletAllocation, the LDA model
-        """
-        # Perform tf-idf
-        vectorizer = TfidfVectorizer()
-        X = vectorizer.fit_transform(self.articles_df['text'])
-
-        # Perform latent dirichlet allocation
-        lda = LatentDirichletAllocation(n_components=nb_topics, random_state=53)
-        lda.fit(X)
-        return lda
 
     def compare_articles(self, article1, article2):
-        # Get the index of the article in the dataframe
-        index1 = self.articles_df.index.get_loc(article1)
-        index2 = self.articles_df.index.get_loc(article2)
-        # Get the lda vector of the two articles
-        lda_vector1 = self.lda.components_[:,index1]
-        lda_vector2 = self.lda.components_[:,index2]
-        # Compute the cosine similarity
-        similarity = np.dot(lda_vector1, lda_vector2) / (np.linalg.norm(lda_vector1) * np.linalg.norm(lda_vector2))
-        return similarity
-        
-    def preprocess_text(self, text, lemmatize = True, remove_stopwords = True, caseFolding = True, normalize= True): 
-        # Convert to lower case
-        if caseFolding:
-            text = text.lower()
-        # Normalize text
-        if normalize:
-            text = self.text_processor.pre_process_doc(text)
-        # Tokenize text
-        if remove_stopwords:
-            text = [word for word in text if word not in self.stop_words]
-        # Lemmatize text
-        if lemmatize:
-            text = [self.lemmatizer.lemmatize(word) for word in text]
-        return ' '.join(text)
+        """
+        @param article1: String, the name of the first article
+        @param article2: String, the name of the second article
+        @return: Float, the cosine similarity between the two articles
+        """
+
+        # Compute the cosine similarity between the two articles
+        embedding1 = self.articles_df.loc[article1]['article_embedding']
+        embedding2 = self.articles_df.loc[article2]['article_embedding']
+        return self.cosine_similarity(embedding1, embedding2)
+
+    def compare_titles(self, article1, article2):
+        """
+        @param article1: String, the name of the first article
+        @param article2: String, the name of the second article
+        @return: Float, the cosine similarity between the two article titles 
+        """
+
+        # Compute the cosine similarity between the two articles
+        embedding1 = self.articles_df.loc[article1]['title_embedding']
+        embedding2 = self.articles_df.loc[article2]['title_embedding']
+        return self.cosine_similarity(embedding1, embedding2)
+    
+    # define a cosine similarity function that gives values between 0 and 1
+    def cosine_similarity(self, v1, v2):
+        return np.abs(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))).round(2)
